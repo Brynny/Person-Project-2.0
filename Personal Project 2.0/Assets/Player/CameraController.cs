a1ef208b22e28c0f3733d67fd3cd3cc1b2f5d214
Brynny <brynnystudios@gmail.com>
2017-07-07 16:39:27 +0800

    save

---
+++ Personal Project 2.0/Assets/Player/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform player;
+	public float distance = 50f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - distance);
+
+		Vector3 playerPosition = GameObject.Find("Player").transform.transform.position;
+		GameObject.Find("Main Camera").transform.position = new Vector3(playerPosition.x, playerPosition.y + distance, playerPosition.z);
+	}
+}
