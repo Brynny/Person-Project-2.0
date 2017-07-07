bd37911659ba8bd413de714a1aa5afaf79224788
Brynny <brynnystudios@gmail.com>
2017-07-07 19:06:56 +0800

    save

---
+++ Personal Project 2.0/Assets/Player/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+	public static GameController GC;
+
+	public static float _playerMoveSpeed = 5f;
+	public static float _playerCurrentHealth;
+	public static float _playerMaxHealth = 20f;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(this);
+		GC = this;
+	}
+
+	void Start () 
+	{
+		_playerCurrentHealth = _playerMaxHealth;
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+}
---
+++ Personal Project 2.0/Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	//public float moveSpeed;
+	private Rigidbody myRB;
+
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+
+	private Camera mainCamera;
+
+
+	void Start ()
+	{
+		myRB = GetComponent<Rigidbody>();
+		mainCamera = FindObjectOfType<Camera>();
+	}
+
+
+	void Update () 
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		Movement();
+	}
+
+	void Movement()
+	{
+		moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+		moveVelocity = moveInput * GameController._playerMoveSpeed;
+
+		myRB.velocity = moveVelocity;
+
+		Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		float rayLength;
+
+		if (groundPlane.Raycast(cameraRay, out rayLength))
+		{
+			Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+			Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+			transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+		}
+	}
+
+}
